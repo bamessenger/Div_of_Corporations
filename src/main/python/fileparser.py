f89c2476d4ed3da5c5d165fd171ecaccbc6f2db8
Brandon <brandon@cedarinsights.com>
2021-02-26 19:13:35 -0500

    add fileparser

--- src/main/python/fileparser.py
+++ src/main/python/fileparser.py
@@ -1,9 +1,24 @@
-from openpyxl import workbook
+from openpyxl import Workbook, load_workbook
+
 
 class ParseFile:
     def __init__(self):
-        self.path =
+        self.filepath = 'C:/Users/brand/OneDrive - InsureGood ' \
+                                 'LLC/Documents - Cedar ' \
+                                 'Insights/applications/WebScrape' \
+                                 '/Div_of_Corporations/SPAC List.xlsx'
+        self.wb = Workbook()
     def fileparser(self):
-        excelFile = 'SPAC List.xlsx'
-        sheetName = 'Sheet1'
-        sheetCols = 'A'
+        # load workbook
+        self.wb = load_workbook(self.filepath)
+        # select workbook
+        self.sheet = self.wb.active
+        # get max row count
+        self.max_row = self.sheet.max_row
+
+        for r in range(2,self.max_row):
+            self.cell = self.sheet.cell(row=r, column=1)
+            print(self.cell.value)
+
+pf = ParseFile()
+pf.fileparser()
