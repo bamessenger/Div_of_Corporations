9412ee8428af2702e0cc71f156e8a9c411bb677e
Brandon <brandon@cedarinsights.com>
2021-03-01 20:01:48 -0500

    def readTable

--- src/main/python/navigation.py
+++ src/main/python/navigation.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from src.main.python.fileparser import ParseFile
 
 # Global variable - change will update all URL instances
@@ -10,12 +11,14 @@ urlMain = 'https://icis.corp.delaware.gov/ecorp/entitysearch/NameSearch.aspx'
 class SiteNavigation:
     def __init__(self):
         # Establish chromedriver path
+        self.options = Options()
+        self.options.add_argument('--headless')
         self.chromedriver_path = 'C:/Users/brand/OneDrive - InsureGood ' \
                                  'LLC/Documents - Cedar ' \
                                  'Insights/applications/WebScrape' \
-                                 '/Div_of_Corporations/' \
-                                 'src/main/python/chromedriver.exe'
-        self.driver = webdriver.Chrome(executable_path=self.chromedriver_path)
+                                 '/Div_of_Corporations/chromedriver.exe'
+        self.driver = webdriver.Chrome(
+            executable_path=self.chromedriver_path, chrome_options=self.options)
         self.excelFile = ParseFile()
 
     def getUrl(self):
@@ -35,10 +38,17 @@ class SiteNavigation:
         self.entityNameSearchBtn.click()
 
     def readTable(self):
-        self.table = self.driver.find_element_by_id('tblResults')
-        self.rows = self.table.find_elements_by_tag_name("tr")
-        corpDict = {}
-        for row in self.rows:
-            keys = row.find_elements_by_tag_name('td')[0]
-            values = row.find_elements_by_tag_name('td')[1]
-
+        self.table = self.driver.find_element_by_xpath("//*[@id='tblResults']")
+        self.rows = self.table.find_elements_by_tag_name('td')
+        for row in range(3,len(self.rows),2):
+            self.rows[row].click()
+            self.driver.implicitly_wait(10)
+            self.fileNumber = self.driver.find_element_by_xpath(
+                '//*[@id="ctl00_ContentPlaceHolder1_lblFileNumber"]')
+            self.entityName = self.driver.find_element_by_id(
+                'ctl00_ContentPlaceHolder1_lblEntityName')
+            self.incDate = self.driver.find_element_by_id(
+                'ctl00_ContentPlaceHolder1_lblIncDate')
+            print(self.fileNumber.text, self.entityName.text, self.incDate.text)
+            self.driver.back()
+            time.sleep(1)
