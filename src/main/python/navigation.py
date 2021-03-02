a076ac3873abf6479e8421d53ea0c01091115c89
Brandon <brandon@cedarinsights.com>
2021-03-02 14:58:37 -0500

    def readTable

--- src/main/python/navigation.py
+++ src/main/python/navigation.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from src.main.python.fileparser import ParseFile
+from selenium.webdriver.support import expected_conditions as ec
 
 # Global variable - change will update all URL instances
 urlMain = 'https://icis.corp.delaware.gov/ecorp/entitysearch/NameSearch.aspx'
@@ -34,21 +35,26 @@ class SiteNavigation:
         # Grab search criteria from Excel file, if query returns results, click
         # on result(s) and save 'File Number' and 'Incorporation Date'
         self.entityNameSearch.send_keys('*merge*')
-        time.sleep(2)
+        self.driver.implicitly_wait(10)
         self.entityNameSearchBtn.click()
 
     def readTable(self):
-        self.table = self.driver.find_element_by_xpath("//*[@id='tblResults']")
+        self.table = self.driver.find_element_by_id('tblResults')
         self.rows = self.table.find_elements_by_tag_name('td')
         for row in range(3,len(self.rows),2):
-            self.rows[row].click()
+            self.table = self.driver.find_element_by_id(
+                'tblResults')
+            self.rows = self.table.find_elements_by_tag_name('td')
             self.driver.implicitly_wait(10)
-            self.fileNumber = self.driver.find_element_by_xpath(
-                '//*[@id="ctl00_ContentPlaceHolder1_lblFileNumber"]')
+            self.entityText = self.rows[row].text
+            self.driver.find_element_by_link_text(self.entityText).click()
+            self.driver.implicitly_wait(10)
+            self.fileNumber = self.driver.find_element_by_id(
+                'ctl00_ContentPlaceHolder1_lblFileNumber')
             self.entityName = self.driver.find_element_by_id(
                 'ctl00_ContentPlaceHolder1_lblEntityName')
             self.incDate = self.driver.find_element_by_id(
                 'ctl00_ContentPlaceHolder1_lblIncDate')
             print(self.fileNumber.text, self.entityName.text, self.incDate.text)
             self.driver.back()
-            time.sleep(1)
+
